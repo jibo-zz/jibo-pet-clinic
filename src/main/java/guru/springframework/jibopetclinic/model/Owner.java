8e3f8789a0ef5c4a0e1c436e00431e82224963fd
Mohamed Omar <mohamed.omar@dpcs.dj>
2022-09-21 13:40:06 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/jibopetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.jibopetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/jibopetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.jibopetclinic.model;
+
+public class Vet extends Person {
+}
